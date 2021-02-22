b05bc1ddd425847707d3c91f54488f2046e7648d
Emily <emi.roca18@gmail.com>
2021-02-22 16:49:06 -0400

    Calculadora completa

---
+++ Tarea1.1/Tarea1.1/Controllers/Calculadora2Controller.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tarea1._1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Calculadora2Controller : ControllerBase
+    {
+        // GET: api/<Calculadora2Controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Calculadora2Controller>/5
+        [HttpGet("{x}/{y}/{operacion}")]
+        public int Get(int x,int y,string operacion)
+        {
+           
+             if (operacion == "suma") 
+            {
+                return (x + y);
+            }
+            else
+            {
+                if (operacion == "resta")
+                {
+                    return (x - y);
+                }
+                else
+                {
+                    if (operacion == "multiplicacion")
+                    {
+                        return (x * y);
+                    }
+                    else
+                    {
+                        if (operacion == "division")
+                        {
+                            return (x / y);
+                        }
+                        else
+                        {
+                            return (0);
+                        }
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
+
+
+
+
